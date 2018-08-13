ccae13b7cb81ebad5dc8a48d4248ff0420d4a319
Sander Wegdam <sanderwegdam@gmail.com>
2018-08-13 03:57:20 -0700

    Wegdam Media

---
+++ wegdam.webege.com/index.php
@@ -0,0 +1,274 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>  
+<title>Wegdam Media Services</title>  
+<link rel="schema.DC" href="http://purl.org/dc/elements/1.1/" />
+<meta name="DC.title" content="Wegdam Media Services" />
+<meta name="DC.Creator" content="Sander Wegdam" />
+<meta name="DC.Description" content="Sander Wegdam is een webdesigner en web developer." />
+<meta name="DC.Publisher" content="Sander Wegdam" />
+<meta name="DC.Contributor" content="Sander Wegdam" />
+<meta name="DC.Language" scheme="DCTERMS.ISO639-3" content="NL" />
+<META NAME="geo.position" CONTENT="latitude; longitude">
+<META NAME="geo.placename" CONTENT="Overdinkel">
+<META NAME="geo.region" CONTENT="NL">
+<meta name="googlebot" content="noodp" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="MobileOptimized" content="240">
+<meta name='HandheldFriendly' content='True'>
+<meta charset="utf-8">
+<meta content="index,follow"/>
+<meta name="DC.Rights" content="Copyright Wegdam 2018"/>
+<meta name="DC.Date" scheme="iso8601" content="01-01-2018"/>
+<meta name="Source" content="Templates"/>
+<meta name="Security" content="Public"/>
+<meta name="Abstract" content="Wegdam Media Services" />
+<meta name="DC.Subject" scheme="_SubjectTaxonomy" content="ZZ999"/>
+<meta name="Owner" content="Sander Wegdam"/>
+<meta name="Robots" content="index,follow"/>
+<link rel="canonical" href="http://www.wegdam.webege.com/index.php" />
+<meta http-equiv="Cache-control" content="Public"/>
+<meta name="Generator" content="Adobe Dreamweaver (http://www.adobe.com)" />
+<link rel="shortlink" href="http://www.wegdam.webege.com/index.php" />
+<link rel="icon" href="img/favicon.png"/>
+<link rel="apple-touch-icon" href="img/favicon.ico"/>
+<link rel="apple-touch-icon" sizes="72x72" href="img/favicon.ico"/>
+<meta name="googlebot" content="noodp" />
+<meta name="web_author" content="Web Developer Sander Wegdam" />
+<meta property="og:title" content="Wegdam Media Services"/>
+<meta property="og:image" content="images"/>
+<meta property="og:description" content="Wegdam Media Services bouwt een website naargelang uw eisenpakket"/>
+<meta property="og:image:alt" content="website laten bouwen" />
+<meta property="og:image:width" content="1200" />
+<meta property="og:image:height" content="630" />
+<meta property="og:site_name" content="Wegdam Media Services" />
+<meta property="og:url" content="https://wegdam.webege.com/" />
+<meta property="og:locale" content="nl_NL" />
+<meta name="twitter:title" content="Wegdamm" />
+<meta name="twitter:site" content="@Wegdamm" />
+<meta name="twitter:creator" content="@Wegdamm" />
+<meta name="rating" content="general" />
+<meta http-equiv="X-UA-Compatible" content="chrome=1" />
+<meta name="contact" content="sanderwegdam@gmail.com" />
+<meta name="reply-to" content="sanderwegdam@gmail.com" />
+<meta name="abstract" content="Wegdam Media Services"/>
+<meta http-equiv="Page-Enter" content="blendTrans(Duration=0.3)"/>
+<meta http-equiv="Page-Exit" content="blendTrans(Duration=0.3)"/>
+<meta name="syndication-source" content="www.sanderwegdam.comuf.com"/>
+<meta scheme="ISBN" name="identifier" content="1-2345-678 X"/>
+<meta name="revisit-after" content="1 day" />
+<meta http-equiv="Resource-Type" content="document"/>
+<meta http-equiv="Window-Target" content="_top"/>
+<meta http-equiv="Set-Cookie" content="ACCOUNT=9983373; path=/; expires=Thursday, 26-Dec-11 00:15:00 GMT"/>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<meta http-equiv="content-language" content="NL"/>
+<meta name="googlebot" content="noodp"/>
+<meta name="author" content="Sander Wegdam"/>
+<meta name="generator" content="dreamweaver"/>
+<meta name="rating" content="general"/>
+<meta name="distribution" content="global"/>
+<meta name="contact" content="sanderwegdam@gmail.com" />
+<meta name="copyright" content="Wegdam Media Services" />
+<meta name="MobileOptimized" content="240">
+<meta name='HandheldFriendly' content='True'>
+
+    <meta name="author" content="Sander Wegdam">
+    <meta name="keywords" content="sander Wegdam, Wegdam, wegdam website, wegdam media, wegdam media services, ontwerp, webdeveloper,
+    losser, website bouwen, website wegdam, twente, mediavormgever, webdesign, seo, responsive website" />
+    <meta name="description" content="Wegdam Media Services bouwt een website naargelang uw eisenpakket." />
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Muli" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/custom.css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body id="page-top">
+    <nav id="mainNav" class="navbar navbar-inverse navbar-fixed-top">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+
+            <a class="navbar-brand" href="#">
+             <img alt="Wegdam Media Logo" src="img/wegdam-media.png" width="140" height="80">
+            </a>
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
+                </button>                
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a class="page-scroll" href="#home">Home</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#services">Services</a>
+                    </li>   
+                    <li>
+                        <a class="page-scroll" target="_blank" href="http://sanderwegdam.comuf.com/">CV</a>
+                    </li>                
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+
+<header id="home">
+<div id="slider-wrapper" class="slider-wrapper">
+    <section id="slider">
+        <article class="slider-item">     
+        <div class="header-content-inner">
+            <h3>Een website bouw ik naargelang uw eisenpakket.</h3>
+            <br>
+            <a href="#services" class="btn btn-outline btn-xl page-scroll">Lees verder</a>
+         </div>
+        </article>
+     </section>        
+    </div>
+  </header>      
+
+<div class="container">
+<div class="row">
+<div class="card flex-md-row mb-4 shadow-sm h-md-250">
+    <div class="col align-self-center mr-3 img-fluid img-thumbnail flex-auto rounded mx-auto">
+        <div class="col-md-12">         
+            <div class="media">
+                <a href="http://sanderwegdam.comuf.com/" target="_blank">
+                <img src="img/sanderwegdam.jpg" alt="Sander Wegdam" class="mr-3 img-fluid img-thumbnail flex-auto rounded mx-auto" style="width:100px"></a>
+            </div>
+             <div class="media-body" style="text-align:center">
+                <h4>Als web developer ontwikkel ik mij graag, <br>
+                door te werken in teamverband.</h4>
+            </div>  
+            <hr>
+          </div>   
+        </div>
+        </div>
+    </div>
+ </div>
+
+    <section id="download" class="download bg-primary text-center">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <h3 class="section-heading">Als webdesigner kan ik u services aanbieden, zoals het 
+                    up to date houden van uw website.</h3>
+                    <p>Ik volg de onwikkelingen op webgebied en mijn expertise geef ik graag door.
+                        <br>Ik werk met webtalen zoals, HTML, Javascript, PHP.</p>
+                    <p>Sander Wegdam helpt u graag bij uw ideeën.</p>
+                    <div class="badges">                                 
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="services" class="features bg-secondary">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <div class="section-heading">
+                        <h2>Een gebruiksvriendelijke website!</h2>
+                        <p class="text-muted">Dat is wat ik u aan kan bieden.</p>
+                        <hr>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="device-container">
+                        <div class="device-mockup iphone6_plus portrait white">
+                            <div class="device">
+                                <div class="screen">
+                                <a href="http://sanderwegdam.comuf.com/" target="_blank">            
+                                    <img src="img/about-me-sanderwegdam.png" class="img-responsive" alt="Sander Wegdam"></a>  
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>           
+
+                <div class="col-md-8">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="feature-item">
+                                    <i class="fa fa-users text-primary" aria-hidden="true"></i>
+                                    <h3>Websitebeheer</h3>
+                                    <p class="text-muted">Wanneer u uw eigen website wilt beheren dan is dat een mogelijkheid.</p>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="feature-item">
+                                    <i class="fa fa-search-plus text-primary" aria-hidden="true"></i>
+                                    <h3>Zoekmachine vriendelijke website</h3>
+                                    <p class="text-muted">Een zoekmachine vriendelijke website die hoog scoort in Google is een must.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="feature-item">
+                                    <i class="fa fa-laptop text-primary" aria-hidden="true"></i>
+                                    <h3>Responsive design</h3>
+                                    <p class="text-muted">Uw website past zich aan de breedte van het beeldscherm,
+                                    en is daarom toegankelijk voor zowel desktop als mobiel.</p>
+                                </div>
+                            </div>
+                                <div class="col-md-6">
+                                    <div class="feature-item">
+                                    <i class="fa fa-check text-primary" aria-hidden="true"></i>
+                                    <h3>Scripttalen</h3>
+                                    <p class="text-muted">Ik werk met de webtalen:
+                                    HTML, CSS, Javascript, PHP. En eventueel ondersteunende frameworks.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </section>
+  <footer>
+    <section id="contact" class="contact">        
+    </section>
+  <footer>
+
+        <?php
+        $query_age = (isset($_GET['pagina']) ? $_GET['pagina'] : null);
+        if(!(isset($_GET['pagina']))) {
+        $_GET['pagina'] = "footer";
+        }        
+        include("pages/". $_GET['pagina'] . ".php");
+        ?>
+
+    <!-- jQuery -->
+    <script src="js/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+
+    <!-- Theme JavaScript -->
+    <script src="js/function.js"></script>
+
+</body>
+</html>
---
+++ wegdam.webege.com/pages/footer.php
@@ -0,0 +1,11 @@
+<div class="col-lg-12 col-md-12 col-xs-12">
+<div class="about-text">
+<a href="//about.me/sanderwegdam" target="blank" class="btn btn-sm btn-secondary-outline btn-primary">About.me</a>
+</div>
+</div>   
+ 
+    <div id="footer_wrapper">
+    <div class="wrapper container">
+        <div id="copyright"><br><br><p>&copy; Wegdam media services</p></div>
+   </div> 
+</div>
